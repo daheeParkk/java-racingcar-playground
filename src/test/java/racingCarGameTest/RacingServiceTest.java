f154e6f771ca0dd0151d0919e0d16a28b214ff25
daheeParkk <qkrekgml7414@naver.com>
2023-03-31 16:20:58 +0900

    test : 위치가 +1되는 테스트 추가

--- src/test/java/racingCarGameTest/RacingServiceTest.java
+++ src/test/java/racingCarGameTest/RacingServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingCarGame.domain.RacingCars;
 import racingCarGame.service.CarService;
 import racingCarGame.service.RacingService;
@@ -11,6 +12,8 @@ import racingCarGame.service.RacingService;
 import static org.assertj.core.api.Assertions.*;
 
 public class RacingServiceTest {
+
+    private static final int MOVED_POSITION_ONCE = 1;
 
     private static RacingService racingService;
     private static CarService carService;
@@ -31,8 +34,17 @@ public class RacingServiceTest {
     @DisplayName("전달받은 차의 수만큼 차를 만드는 테스트")
     @ParameterizedTest
     @CsvSource(value = {"car1,car2,car3=3","car1,car2=2"}, delimiter = '=')
-    public void generateCarsTest(String carsName, int numberOfCars) {
-        RacingCars racingCars = carService.generateCars(carsName);
+    public void generateCarsTest(String carsNames, int numberOfCars) {
+        RacingCars racingCars = carService.generateCars(carsNames);
         assertThat(racingCars.getNumberOfCars()).isEqualTo(numberOfCars);
+    }
+
+    @DisplayName("위치가 +1되는 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"car1,car2,car3>car1", "car1,car2,car3>car2"}, delimiter = '>')
+    public void movePositionTest(String carsNames, String carName) {
+        RacingCars racingCars = carService.generateCars(carsNames);
+        carService.moveCar(carName);
+        assertThat(racingCars.getPosition(carName)).isEqualTo(MOVED_POSITION_ONCE);
     }
 }
