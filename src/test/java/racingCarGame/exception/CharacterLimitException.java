92347305cf639b05189e1f612b22ef42822c22b0
daheeParkk <qkrekgml7414@naver.com>
2023-04-01 03:59:08 +0900

    docs : 자동차 이름이 5글자를 초과할 경우 테스트 추가

---
+++ src/test/java/racingCarGame/exception/CharacterLimitException.java
@@ -0,0 +1,8 @@
+package racingCarGame.exception;
+
+public class CharacterLimitException extends RuntimeException{
+
+    public CharacterLimitException() {
+        super();
+    }
+}
--- src/test/java/racingCarGameTest/exceptionTest.java
+++ src/test/java/racingCarGameTest/exceptionTest.java
@@ -1,2 +1,30 @@
-package racingCarGameTest;public class exceptionTest {
+package racingCarGameTest;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingCarGame.exception.CharacterLimitException;
+import racingCarGame.service.CarService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class exceptionTest {
+
+    private static CarService carService;
+
+    @BeforeAll
+    public static void generateService() {
+        carService = new CarService();
+    }
+
+    @DisplayName("자동차 이름이 5글자를 초과할 경우")
+    @ParameterizedTest
+    @ValueSource(strings = {"abcde", "abcdef"})
+    public void characterLimitException(String carsNames) {
+
+        assertThatThrownBy(() -> carService.generateCars(carsNames)).isInstanceOf(CharacterLimitException.class);
+    }
 }
