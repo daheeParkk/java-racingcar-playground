633e0454c7a38d2d0fe888163f4c48ff5be79b29
daheeParkk <qkrekgml7414@naver.com>
2023-04-01 02:42:08 +0900

    refactor : 결과를 출력하는 기능 구현

--- src/main/java/racingCarGame/view/ResultView.java
+++ src/main/java/racingCarGame/view/ResultView.java
@@ -1,2 +1,24 @@
-package racingCarGame.view;public class ResultView {
+package racingCarGame.view;
+
+import racingCarGame.domain.RacingCars;
+import racingCarGame.service.CarService;
+
+import java.util.List;
+
+public class ResultView {
+
+    private final CarService carService = new CarService();
+
+    public void outputExecutionResult(RacingCars racingCars) {
+        System.out.println("실행 결과");
+        for (int i=0; i< racingCars.getNumberOfCars(); i++) {
+            String carName = racingCars.getNameByIndex(i);
+            System.out.println(carName + " : " + carService.getStick(carName) + "\n");
+        }
+    }
+
+    public void outputWinningCar(List<String> winningCars) {
+        String winningCar = winningCars.toString().substring(1, winningCars.size());
+        System.out.print(winningCar + "가 최종 우승했습니다.");
+    }
 }
