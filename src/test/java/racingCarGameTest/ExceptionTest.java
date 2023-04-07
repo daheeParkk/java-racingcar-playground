c6da3f6d5cfc4814de479c4852f983530858a1ad
daheeParkk <qkrekgml7414@naver.com>
2023-04-08 06:02:16 +0900

    test : ExceptionTest 수정

--- src/test/java/racingCarGameTest/ExceptionTest.java
+++ src/test/java/racingCarGameTest/ExceptionTest.java
@@ -1,36 +1,29 @@
 package racingCarGameTest;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingCarGame.domain.RacingCars;
 import racingCarGame.exception.CharacterLimitException;
 import racingCarGame.exception.DuplicateException;
-import racingCarGame.service.CarService;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ExceptionTest {
 
-    private static CarService carService;
-
-    @BeforeAll
-    public static void generateService() {
-        carService = new CarService();
-    }
-
     @DisplayName("자동차 이름이 5글자를 초과할 경우")
     @ParameterizedTest
     @ValueSource(strings = {"abcdef,abcdefg", "ab,abc,abcdef"})
     public void characterLimitExceptionTest(String carsNames) {
-        assertThatThrownBy(() -> carService.generateCars(carsNames)).isInstanceOf(CharacterLimitException.class);
+        assertThatThrownBy(() -> RacingCars.from(carsNames)).
+                isInstanceOf(CharacterLimitException.class);
     }
 
     @DisplayName("자동차 이름이 중복일 경우")
     @ParameterizedTest
     @ValueSource(strings = {"abc,abc,qwe", "dahee,hui,dahee", "bob,bob"})
     public void DuplicateExceptionTest(String carsNames) {
-        assertThatThrownBy(() -> carService.generateCars(carsNames))
+        assertThatThrownBy(() -> RacingCars.from(carsNames))
                 .isInstanceOf(DuplicateException.class);
     }
 }
